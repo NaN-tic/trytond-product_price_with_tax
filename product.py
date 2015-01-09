1a0015b76fdbf70e99192736628c0a55186a923f
jmartin <jmartin@zikzakmedia.com>
2015-01-09 15:51:58 +0100

    Fix depends and field names passed as parameter

--- product.py
+++ product.py
@@ -28,12 +28,12 @@ class Template:
 
     def get_list_price_with_tax(self):
         Tax = Pool().get('account.tax')
-        taxes = [Tax(t) for t in self.get_taxes('customer_taxes')]
+        taxes = [Tax(t) for t in self.get_taxes('customer_taxes_used')]
         taxes = Tax.compute(taxes, self.list_price, 1.0)
         tax_amount = sum([t['amount'] for t in taxes], Decimal('0.0'))
         return self.list_price + tax_amount
 
-    @fields.depends('list_price')
+    @fields.depends('taxes_category', 'list_price')
     def on_change_list_price(self):
         try:
             changes = super(Template, self).on_change_list_price()
@@ -45,11 +45,11 @@ class Template:
 
     def get_list_price(self):
         Tax = Pool().get('account.tax')
-        taxes = [Tax(t) for t in self.get_taxes('customer_taxes')]
+        taxes = [Tax(t) for t in self.get_taxes('customer_taxes_used')]
         tax_amount = Tax.reverse_compute(self.list_price_with_tax, taxes)
         return tax_amount.quantize(Decimal(str(10.0 ** -DIGITS)))
 
-    @fields.depends('list_price_with_tax')
+    @fields.depends('taxes_category', 'list_price_with_tax')
     def on_change_list_price_with_tax(self):
         changes = {}
         if self.list_price_with_tax:
@@ -58,12 +58,12 @@ class Template:
 
     def get_cost_price_with_tax(self):
         Tax = Pool().get('account.tax')
-        taxes = [Tax(t) for t in self.get_taxes('supplier_taxes')]
+        taxes = [Tax(t) for t in self.get_taxes('supplier_taxes_used')]
         taxes = Tax.compute(taxes, self.cost_price, 1.0)
         tax_amount = sum([t['amount'] for t in taxes], Decimal('0.0'))
         return self.cost_price + tax_amount
 
-    @fields.depends('cost_price')
+    @fields.depends('taxes_category', 'cost_price')
     def on_change_cost_price(self):
         try:
             changes = super(Template, self).on_change_cost_price()
@@ -75,11 +75,11 @@ class Template:
 
     def get_cost_price(self):
         Tax = Pool().get('account.tax')
-        taxes = [Tax(t) for t in self.get_taxes('supplier_taxes')]
+        taxes = [Tax(t) for t in self.get_taxes('supplier_taxes_used')]
         tax_amount = Tax.reverse_compute(self.cost_price_with_tax, taxes)
         return tax_amount.quantize(Decimal(str(10.0 ** -DIGITS)))
 
-    @fields.depends('cost_price_with_tax')
+    @fields.depends('taxes_category', 'cost_price_with_tax')
     def on_change_cost_price_with_tax(self):
         changes = {}
         if self.cost_price_with_tax:
