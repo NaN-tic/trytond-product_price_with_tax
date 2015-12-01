bd7da305eb3d5f1f4d91c39dc5bb0d551bc8e066
resteve <resteve@zikzakmedia.com>
2015-12-01 11:14:36 +0100

    Tests

--- tests/test_product_price_with_tax.py
+++ tests/test_product_price_with_tax.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of product_price_with_tax module for Tryton.
+# This file is part of the product_price_with_tax module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductPriceWithTaxTestCase(unittest.TestCase):
+class ProductPriceWithTaxTestCase(ModuleTestCase):
     'Test Product Price With Tax module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_price_with_tax')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_price_with_tax')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_price_with_tax'
 
 
 def suite():
